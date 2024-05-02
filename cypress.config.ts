ee7b2b5dc9c6496a31d1da5a1a30eeb04f7f7329
mafeZapata <49245929+mafeZapata@users.noreply.github.com>
2024-05-02 16:03:48 -0500

    solving eslint problems (#6)

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,9 +1,11 @@
-const { defineConfig } = require("cypress");
+import { defineConfig } from "cypress";
 
-module.exports = defineConfig({
-  e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+   export default defineConfig({
+    e2e: {
+     chromeWebSecurity: false,
+     setupNodeEvents(on, config) {
+       // e2e testing node events setup code
+       return config;
+      },
     },
-  },
-});
+  });
