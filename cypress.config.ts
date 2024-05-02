ea2e8c53bb8cf32f628eeaf7127d92d86eb029a5
mafeZapata <mafez0427@gmail.com>
2024-05-02 16:34:50 -0500

    config ci

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,9 +2,14 @@ import { defineConfig } from "cypress";
 
    export default defineConfig({
     e2e: {
-     chromeWebSecurity: false,
-     setupNodeEvents(on, config) {
-       // e2e testing node events setup code
+      video: false,
+      screenshotOnRunFailure: false,
+      chromeWebSecurity: false,
+      setupNodeEvents(on, config) {
+           // e2e testing node events setup code
+           config.defaultCommandTimeout= 10000;
+           config.responseTimeout= 20000;
+           config.pageLoadTimeout= 60000;
        return config;
       },
     },
